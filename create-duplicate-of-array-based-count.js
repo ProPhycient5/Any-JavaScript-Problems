aa00da4b8b715ec0a71f47d6ba7d0e2cd629d073
Sawan Kuma Das <71059909+ProPhycient5@users.noreply.github.com>
2022-02-24 18:08:01 +0530

    duplicate-function
    Create a function that will generate duplicate based on its input array and its count.
    // input: duplicate([1,2,3,4,5], 2);
    
    //output: [1,2,3,4,5,1,2,3,4,5,1,2,3,4,5]

---
+++ create-duplicate-of-array-based-count.js
@@ -0,0 +1,23 @@
+// Question: Create a function named as duplicate which has two parameters, one is array and 
+// other is count(i.e. how many duplicate you want).
+// input: duplicate([1,2,3,4,5], 2);
+
+//output: [1,2,3,4,5,1,2,3,4,5,1,2,3,4,5]
+
+function duplicate(arr, count){
+	const originalLength= arr.length;
+	for (let i=0; i < (count * originalLength); i++){
+		arr.push(arr[i]);
+	}
+	return arr;
+}
+
+//or 
+
+function duplicate(arr, count){
+	  let newArr = [...arr];
+	for (let i=0; i < count; i++){
+		newArr.push(...arr);
+	}
+	return newArr;
+}
